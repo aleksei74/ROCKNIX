c17f46db4626e90b569b62957970598cacef047e
byunjaeil <byunjaeil@gmail.com>
2024-05-21 21:41:48 +0900

    Revert "Cleanup"
    This reverts commit 0f118d0164118b150bd57053e65d04a58678c152.

--- packages/virtual/misc-packages/package.mk
+++ packages/virtual/misc-packages/package.mk
@@ -15,3 +15,9 @@ if [ ! "${BASE_ONLY}" = "true" ]
 then
   PKG_DEPENDS_TARGET+=" ${ADDITIONAL_PACKAGES}"
 fi
+
+# Entware support
+if [ "$ENTWARE_SUPPORT" = "yes" ]; then
+  ln -sf /storage/.opt ${INSTALL}/opt
+  PKG_DEPENDS_TARGET+=" entware"
+fi
