9f737df9459b7b5e8ff611aa5f6deab58ecfa616
byunjaeil <byunjaeil@gmail.com>
2024-05-22 10:50:14 +0900

    british-choi's scummvm

--- packages/emulators/standalone/scummvmsa/package.mk
+++ packages/emulators/standalone/scummvmsa/package.mk
@@ -3,9 +3,9 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="scummvmsa"
-PKG_VERSION="b347be081cb35889bebc191165fa88f5261932ba"
+PKG_VERSION="6378050357807502d47a5291cf53a38f4a18d9d0"
 PKG_LICENSE="GPL2"
-PKG_SITE="https://github.com/scummvm/scummvm"
+PKG_SITE="https://github.com/british-choi/scummvm"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain SDL2 SDL2_net freetype fluidsynth soundfont-generaluser pipewire"
 PKG_LONGDESC="Script Creation Utility for Maniac Mansion Virtual Machine"
