7fd7102508d12a7a05444830b917509f5a76acd3
aleksei74 <aleksei74@gmail.com>
2024-08-17 19:05:25 +0900

    ES Emulator 에 fbneo-dsno 추가
    기존에 소스 수정하신거 보니 
      add_emu_core cps1 retroarch fbneo true
      add_emu_core cps1 retroarch fbneo-dsno false <-이부분도 true로 해놓으셨더군요.
    우선 true값은 디폴트값이라 각항목당 1개만 선택되는거 같네요.
    
    그래서 일단 추가된 fbneo-dsno코어는 전부 false로 설정했습니다.

--- packages/virtual/emulators/package.mk
+++ packages/virtual/emulators/package.mk
@@ -24,7 +24,7 @@ LIBRETRO_CORES="81-lr a5200-lr arduous-lr atari800-lr beetle-gba-lr beetle-lynx-
                 snes9x2002-lr snes9x2005_plus-lr snes9x2010-lr stella-lr swanstation-lr tgbdual-lr theodore-lr tic80-lr uzem-lr       \
                 vba-next-lr vbam-lr vecx-lr vice-lr vircon32-lr virtualjaguar-lr xmil-lr yabasanshiro-lr"
 
-LIBRETRO_CORES+=" fbneo-dsno-lr"
+LIBRETRO_CORES+=" -dsno-lr"
 
 ### Emulators or cores for specific devices
 case "${DEVICE}" in
@@ -181,6 +181,7 @@ makeinstall_target() {
   add_emu_core arcade retroarch mame2010 false
   add_emu_core arcade retroarch mame2015 false
   add_emu_core arcade retroarch fbneo false
+  add_emu_core arcade retroarch fbneo-dsno false
   add_emu_core arcade retroarch fbalpha2012 false
   add_emu_core arcade retroarch fbalpha2019 false
   add_emu_core arcade retroarch mame false
@@ -273,6 +274,7 @@ makeinstall_target() {
 
   ### Capcom Playsystem 1
   add_emu_core cps1 retroarch fbneo true
+  add_emu_core cps1 retroarch fbneo-dsno false
   add_emu_core cps1 retroarch mame2003_plus false
   add_emu_core cps1 retroarch mame2010 false
   add_emu_core cps1 retroarch fbalpha2012 false
@@ -285,6 +287,7 @@ makeinstall_target() {
 
   ### Capcom Playsystem 2
   add_emu_core cps2 retroarch fbneo true
+  add_emu_core cps2 retroarch fbneo-dsno false
   add_emu_core cps2 retroarch mame2003_plus false
   add_emu_core cps2 retroarch mame2010 false
   add_emu_core cps2 retroarch fbalpha2012 false
@@ -297,6 +300,7 @@ makeinstall_target() {
 
   ### Capcom Playsystem 3
   add_emu_core cps3 retroarch fbneo true
+  add_emu_core cps3 retroarch fbneo-dsno false
   add_emu_core cps3 retroarch mame2003_plus false
   add_emu_core cps3 retroarch mame2010 false
   add_emu_core cps3 retroarch fbalpha2012 false
@@ -367,6 +371,7 @@ makeinstall_target() {
 
   ### Final Burn Neo
   add_emu_core fbn retroarch fbneo true
+  add_emu_core fbn retroarch fbneo-dsno false
   add_emu_core fbn retroarch mame2003_plus false
   add_emu_core fbn retroarch mame2010 false
   add_emu_core fbn retroarch mame2015 false
@@ -587,6 +592,7 @@ makeinstall_target() {
   add_emu_core mame retroarch mame2015 false
   add_emu_core mame retroarch mame false
   add_emu_core mame retroarch fbneo false
+  add_emu_core mame retroarch fbneo-dsno false
   add_emu_core mame retroarch fbalpha2012 false
   add_emu_core mame retroarch fbalpha2019 false
   add_es_system mame
@@ -662,6 +668,7 @@ makeinstall_target() {
 
   ### SNK NeoGeo
   add_emu_core neogeo retroarch fbneo true
+  add_emu_core neogeo retroarch fbneo-dsno false
   add_emu_core neogeo retroarch mame2003_plus false
   add_emu_core neogeo retroarch fbalpha2012 false
   add_emu_core neogeo retroarch fbalpha2019 false
