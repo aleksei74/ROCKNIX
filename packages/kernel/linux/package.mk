5a11ea235cfa10bf9d8d49b66b11fc43998d9425
uzucore <byunjaeil@gmail.com>
2024-08-27 16:31:56 +0900

    Update package.mk

--- packages/kernel/linux/package.mk
+++ packages/kernel/linux/package.mk
@@ -34,6 +34,11 @@ case ${DEVICE} in
     PKG_URL="https://git.sr.ht/~tokyovigilante/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS+=" mainline"
     ;;
+   AMD64)
+    PKG_VERSION="6.10.4"
+    PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+    PKG_PATCH_DIRS+=" mainline"
+    ;;
   *)
     PKG_VERSION="6.9.12"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
