acf615346ad752061d41a56a11519bc240cc1116
Gianni Spadoni <me@gio.blue>
2026-04-26 03:44:11 +0800

    steam: support SM8750
    Forgot to add this one
    
    Signed-off-by: Gianni Spadoni <me@gio.blue>

--- projects/ROCKNIX/packages/virtual/emulators/package.mk
+++ projects/ROCKNIX/packages/virtual/emulators/package.mk
@@ -696,7 +696,7 @@ makeinstall_target() {
 
   ## Steam
   case ${DEVICE} in
-    SM8250|SM8550|SM8650)
+    SM8250|SM8550|SM8650|SM8750)
       add_emu_core steam steam steam true
       install_script "Start Steam x86.sh"
       install_script "Start Steam ARM64.sh"
