b55de7ce1c6a91eb4c9b0252dda6ad639fc5520d
aleksei74 <aleksei74@gmail.com>
2026-07-17 21:57:15 +0900

    armsx2-sa: update to 2.6.1.2

--- projects/ROCKNIX/packages/emulators/standalone/armsx2-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/armsx2-sa/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2026-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="armsx2-sa"
-PKG_VERSION="9e0dc436f7bdd5aae08a94e5b69cb640302ab50f" # tag: 2.6.0
+PKG_VERSION="02b854c5b79a8910e1492d2eb0aa80693ac4d0af" # tag: 2.6.1.2
 PKG_ARCH="aarch64"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/ARMSX2/ARMSX2"
