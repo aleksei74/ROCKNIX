67ba6a3191dfb76737130b84e4dcced4390d5d7f
aleksei74 <aleksei74@gmail.com>
2026-08-05 12:59:59 +0900

    xenia-sa: Switch standalone emulator from Canary to Edge (has207/xenia-edge)

--- projects/ROCKNIX/packages/emulators/standalone/xenia-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/xenia-sa/package.mk
@@ -2,24 +2,26 @@
 # Copyright (C) 2026-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="xenia-sa"
-PKG_VERSION="7010c86fb14f118ee598d3f76010dc0759b9502a"
+PKG_VERSION="e25bfd8242576532c8f530a5aaaccafd9badf7cd"
 PKG_LICENSE="BSD-3-Clause"
-PKG_SITE="https://github.com/xenia-canary/xenia-canary"
+PKG_SITE="https://github.com/has207/xenia-edge"
 PKG_URL="${PKG_SITE}.git"
-PKG_GIT_CLONE_BRANCH="canary_experimental"
+PKG_GIT_CLONE_BRANCH="edge"
 PKG_GIT_SUBMODULE_DEPTH="1"
 # Linux/AArch64 dependencies only. Excludes Windows DirectX, x64 JIT, tests,
 # lint tooling and the unused portal backend.
 PKG_GIT_SUBMODULES="third_party/FFmpeg third_party/FidelityFX-CAS third_party/FidelityFX-FSR \
-                    third_party/SDL2 third_party/VulkanMemoryAllocator \
-                    third_party/aes_128 third_party/capstone \
-                    third_party/cxxopts third_party/date third_party/discord-rpc \
-                    third_party/disruptorplus third_party/fmt third_party/glslang \
-                    third_party/imgui third_party/pugixml third_party/rapidcsv \
-                    third_party/rapidjson third_party/snappy third_party/tabulate \
-                    third_party/tomlplusplus third_party/utfcpp third_party/xbyak_aarch64 \
-                    third_party/xxhash third_party/zarchive third_party/zlib-ng third_party/zstd"
-PKG_LONGDESC="Xenia Canary - Xbox 360 emulator (experimental AArch64 build)."
+                    third_party/SDL3 third_party/SPIRV-Headers third_party/SPIRV-Tools \
+                    third_party/VulkanMemoryAllocator third_party/aes_128 \
+                    third_party/asio third_party/boost_context/context \
+                    third_party/capstone third_party/cxxopts third_party/date \
+                    third_party/discord-rpc third_party/disruptorplus third_party/fmt \
+                    third_party/glslang third_party/imgui third_party/miniaudio \
+                    third_party/pugixml third_party/rapidjson third_party/snappy \
+                    third_party/tomlplusplus third_party/utfcpp third_party/wxWidgets \
+                    third_party/xbyak_aarch64 third_party/xxhash third_party/zarchive \
+                    third_party/zlib-ng third_party/zstd"
+PKG_LONGDESC="Xenia Edge - Xbox 360 emulator (experimental AArch64 build)."
 PKG_DEPENDS_TARGET="toolchain llvm:host Python3:host glslang:host spirv-tools spirv-tools:host \
                     vulkan-loader vulkan-headers gtk3 SDL2 alsa-lib lz4 xwayland"
 PKG_TOOLCHAIN="manual"
@@ -44,6 +46,14 @@ make_target() {
       ;;
   esac
 
+  mkdir -p "${BUILD_DIR}"
+  (cd "${PKG_BUILD}" && git submodule update --init --recursive third_party/wxWidgets) || return 1
+  python3 -c "import runpy; runpy.run_path('${PKG_BUILD}/xenia-build.py')['generate_version_h']('${BUILD_DIR}')" || return 1
+  (cd "${PKG_BUILD}" && python3 xenia-build.py slang) || return 1
+
+  # Compile host xenia-shader-cc tool natively for x86_64 build host
+  (cd "${PKG_BUILD}" && rm -rf build-host && CFLAGS="" CXXFLAGS="" ASMFLAGS="" LDFLAGS="" PATH="/usr/bin:${PATH}" cmake -S . -B build-host -DBUILD_HOST_TOOLS_ONLY=ON -DXENIA_ENABLE_LTO=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ && PATH="/usr/bin:${PATH}" cmake --build build-host --target xenia-shader-cc -j$(nproc)) || return 1
+
   for _v in CFLAGS CXXFLAGS ASMFLAGS; do
     export ${_v}="$(echo ${!_v} | sed -e "${FLAGS_CLEAN}") ${TARGET_FLAGS} ${OPT_FLAGS} ${CPU_FLAGS}"
   done
@@ -58,9 +68,6 @@ make_target() {
   # Shader generation must use host executables during an AArch64 cross build.
   export PATH="${TOOLCHAIN}/bin:${PATH}"
   unset VULKAN_SDK
-
-  mkdir -p "${BUILD_DIR}"
-  python3 -c "import runpy; runpy.run_path('${PKG_BUILD}/xenia-build.py')['generate_version_h']('${BUILD_DIR}')" || return 1
 
   cmake -G Ninja \
     -S "${PKG_BUILD}" \
@@ -96,7 +103,7 @@ make_target() {
 }
 
 makeinstall_target() {
-  local XENIA_BIN="${PKG_BUILD}/.${TARGET_NAME}/bin/Linux/xenia_canary"
+  local XENIA_BIN="${PKG_BUILD}/.${TARGET_NAME}/bin/Linux/xenia_edge"
 
   if [ ! -x "${XENIA_BIN}" ]; then
     echo "Missing Xenia executable: ${XENIA_BIN}" >&2
@@ -104,9 +111,9 @@ makeinstall_target() {
   fi
 
   mkdir -p "${INSTALL}/usr/bin"
-  cp -v "${XENIA_BIN}" "${INSTALL}/usr/bin/xenia_canary"
+  cp -v "${XENIA_BIN}" "${INSTALL}/usr/bin/xenia_edge"
   cp -v "${PKG_DIR}/scripts/start_xenia.sh" "${INSTALL}/usr/bin/"
   mkdir -p "${INSTALL}/usr/config/xenia"
-  cp -v "${PKG_DIR}/config/xenia-canary.config.toml" "${INSTALL}/usr/config/xenia/"
-  chmod 0755 "${INSTALL}/usr/bin/xenia_canary" "${INSTALL}/usr/bin/start_xenia.sh"
+  cp -v "${PKG_DIR}/config/xenia-edge.config.toml" "${INSTALL}/usr/config/xenia/"
+  chmod 0755 "${INSTALL}/usr/bin/xenia_edge" "${INSTALL}/usr/bin/start_xenia.sh"
 }
