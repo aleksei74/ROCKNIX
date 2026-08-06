de50f784b2cd60f7270967ad2be6fad11a6b7ea6
aleksei74 <aleksei74@gmail.com>
2026-08-06 20:41:47 +0900

    fix(rpcs3-sa): use latest source build recipe from UzuCore

--- projects/ROCKNIX/packages/emulators/standalone/rpcs3-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/rpcs3-sa/package.mk
@@ -2,28 +2,54 @@
 # Copyright (C) 2024 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="rpcs3-sa"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/RPCS3/rpcs3-binaries-linux"
-PKG_DEPENDS_TARGET="toolchain libevdev SDL2 qt6 mesa libcom-err"
-PKG_LONGDESC="PS3 Emulator appimage"
-PKG_TOOLCHAIN="manual"
-PKG_VERSION="f3f52feddfaf4f978b5a519ea1f97cc1a90a479a"
-PKG_REL_VERSION="0.0.42-19698-f3f52fed"
+PKG_LICENSE="GPLv2"
+PKG_LONGDESC="PS3 Emulator"
+PKG_VERSION="59e59c7de6e5595c7f9a795489b2570273aafb0d"
+PKG_SITE="https://github.com/RPCS3/rpcs3"
+PKG_URL="${PKG_SITE}.git"
+PKG_TOOLCHAIN="cmake"
+PKG_DEPENDS_TARGET="toolchain llvm qt6 SDL3 ffmpeg curl zlib zstd libpng pugixml \
+                    libusb libevdev alsa-lib pulseaudio openal-soft miniupnpc"
 
-case ${TARGET_ARCH} in
-  x86_64)
-    PKG_URL="${PKG_SITE}/releases/download/build-${PKG_VERSION}/rpcs3-v${PKG_REL_VERSION}_linux64.AppImage"
-  ;;
-  aarch64)
-    PKG_URL="${PKG_SITE}-arm64/releases/download/build-${PKG_VERSION}/rpcs3-v${PKG_REL_VERSION}_linux_aarch64.AppImage"
-  ;;
-esac
+if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGL} glew"
+fi
+if [ "${VULKAN_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${VULKAN} vulkan-headers"
+fi
+
+pre_configure_target() {
+  # llvm is linked from the sysroot, drop its build tree to free disk for rpcs3
+  rm -rf "$(get_build_dir llvm)"
+
+  export CFLAGS="${CFLAGS} -DGLEW_EGL"
+  export CXXFLAGS="${CXXFLAGS} -DGLEW_EGL"
+
+  PKG_CMAKE_OPTS_TARGET+=" -DWITH_LLVM=ON \
+                           -DBUILD_LLVM=OFF \
+                           -DSTATIC_LINK_LLVM=OFF \
+                           -DLLVM_DIR=${SYSROOT_PREFIX}/usr/lib/cmake/llvm \
+                           -DUSE_NATIVE_INSTRUCTIONS=OFF \
+                           -DUSE_PRECOMPILED_HEADERS=OFF \
+                           -DUSE_LTO=ON \
+                           -DUSE_DISCORD_RPC=OFF \
+                           -DUSE_SYSTEM_FFMPEG=ON \
+                           -DUSE_SYSTEM_CURL=ON \
+                           -DUSE_SYSTEM_SDL=ON \
+                           -DUSE_SYSTEM_ZLIB=ON \
+                           -DUSE_SYSTEM_ZSTD=ON \
+                           -DUSE_SYSTEM_LIBPNG=ON \
+                           -DUSE_SYSTEM_PUGIXML=ON \
+                           -DUSE_SYSTEM_LIBUSB=ON \
+                           -DUSE_SYSTEM_OPENAL=ON \
+                           -DUSE_SYSTEM_MINIUPNPC=ON \
+                           -DUSE_SYSTEM_OPENCV=OFF"
+}
 
 makeinstall_target() {
-  # Redefine strip or the AppImage will be stripped rendering it unusable.
-  export STRIP=true
   mkdir -p ${INSTALL}/usr/bin
-  cp ${PKG_BUILD}/${PKG_NAME}-${PKG_VERSION}.AppImage ${INSTALL}/usr/bin/${PKG_NAME}
+  DESTDIR=${INSTALL} cmake --install ${PKG_BUILD}/.${TARGET_NAME}
+  mv ${INSTALL}/usr/bin/rpcs3 ${INSTALL}/usr/bin/${PKG_NAME}
   cp -rf ${PKG_DIR}/scripts/start_rpcs3.sh ${INSTALL}/usr/bin
   chmod 755 ${INSTALL}/usr/bin/*
   mkdir -p ${INSTALL}/usr/config/rpcs3
